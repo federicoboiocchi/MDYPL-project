fc113e5e39245ab9585c97eefa929a8401e69991
Ioannis Kosmidis <ioannis.kosmidis@warwick.ac.uk>
2025-07-30 11:49:27 +0100

    plogis -> plogis2, as plogis is in base R

--- SE-intercept.R
+++ SE-intercept.R
@@ -20,62 +20,57 @@
 #'     operator; default is `1e-10`. fixed point problem solved via Newton-Raphson
 
 mdypl_se4 <- function(mu, b, sigma, iota, kappa, gamma, alpha, intercept, gh = NULL, prox_tol = 1e-10) {
-  if (is.null(gh)) {
-    gh <- gauss.quad(200, kind = "hermite")
-  }
+    if (is.null(gh))
+        gh <- gauss.quad(200, kind = "hermite")
+    xi <- gh$nodes
+    wi <- gh$weights
+    n_quad <- length(xi)
+    xi1 <- rep(xi, times = n_quad)
+    xi2 <- rep(xi, each = n_quad)
+    w2 <- rep(wi, times = n_quad) * rep(wi, each = n_quad)
 
-  xi <- gh$nodes
-  wi <- gh$weights
+    plogis2 <- function(x) (1 + exp(-x))^(-1)
 
-  n_quad <- length(xi)
-  xi1 <- rep(xi, times = n_quad)
-  xi2 <- rep(xi, each = n_quad)
-  w2 <- rep(wi, times = n_quad) * rep(wi, each = n_quad)
-
-  # proximal operator function via Newton-Raphson
-
-  plogis <- function(x) (1 + exp(-x))^(-1)
-
-  prox <- function(x, b) {
-    u <- 0
-    g0 <- x - b / 2
-    while (!all(abs(g0) < prox_tol)) {
-      pr <- plogis(u)
-      g0 <- x - u - b * pr
-      u <- u + g0 / (b * pr * (1 - pr) + 1)
+    prox <- function(x, b) {
+        u <- 0
+        g0 <- x - b / 2
+        while (!all(abs(g0) < prox_tol)) {
+            pr <- plogis2(u)
+            g0 <- x - u - b * pr
+            u <- u + g0 / (b * pr * (1 - pr) + 1)
+        }
+        u
     }
-    u
-  }
 
-  sqrt2 <- sqrt(2)
+    sqrt2 <- sqrt(2)
 
-  q1 <- xi1 * sqrt2
+    q1 <- xi1 * sqrt2
 
-  z <- gamma * q1 + intercept
-  zs <- mu * gamma * xi1 * sqrt2 + sqrt(kappa) * sigma * xi2 * sqrt2 + iota
-  p_z <- plogis(z)
-  p_z_n <- plogis(-z)
-  a_frac <- 0.5 * (1 + alpha)
+    z <- gamma * q1 + intercept
+    zs <- mu * gamma * xi1 * sqrt2 + sqrt(kappa) * sigma * xi2 * sqrt2 + iota
+    p_z <- plogis2(z)
+    p_z_n <- plogis2(-z)
+    a_frac <- 0.5 * (1 + alpha)
 
-  prox_val <- prox(a_frac * b + zs, b)
-  p_prox_val <- plogis(prox_val)
+    prox_val <- prox(a_frac * b + zs, b)
+    p_prox_val <- plogis2(prox_val)
 
-  prox_val_n <- prox(a_frac * b - zs, b)
-  p_prox_val_n <- plogis(prox_val_n)
+    prox_val_n <- prox(a_frac * b - zs, b)
+    p_prox_val_n <- plogis2(prox_val_n)
 
-  prox_resid <- a_frac - p_prox_val
-  prox_resid_n <- a_frac - p_prox_val_n
+    prox_resid <- a_frac - p_prox_val
+    prox_resid_n <- a_frac - p_prox_val_n
 
-  # four equations of the nonlinear system
+    ## four equations of the nonlinear system
 
-  v <- w2 * (1 / pi)
-  out <- c(
-    sum(v * z * (p_z * (prox_resid) - p_z_n * (prox_resid_n))),
-    sum(v * ((p_z / (1 + b * p_prox_val * (1 - p_prox_val))) + (p_z_n / (1 + b * p_prox_val_n * (1 - p_prox_val_n))))) - 1 + kappa,
-    sum(v * ((p_z * (prox_resid)^2) + p_z_n * (prox_resid_n)^2)) * (b / kappa)^2 - sigma^2,
-    sum(v * ((p_z * (prox_resid)) - (p_z_n * (prox_resid_n))))
-  )
-  out
+    v <- w2 * (1 / pi)
+    out <- c(
+        sum(v * z * (p_z * (prox_resid) - p_z_n * (prox_resid_n))),
+        sum(v * ((p_z / (1 + b * p_prox_val * (1 - p_prox_val))) + (p_z_n / (1 + b * p_prox_val_n * (1 - p_prox_val_n))))) - 1 + kappa,
+        sum(v * ((p_z * (prox_resid)^2) + p_z_n * (prox_resid_n)^2)) * (b / kappa)^2 - sigma^2,
+        sum(v * ((p_z * (prox_resid)) - (p_z_n * (prox_resid_n))))
+    )
+    out
 }
 
 #' Solving the MDYPL state evolution equations.
@@ -92,21 +87,21 @@ mdypl_se4 <- function(mu, b, sigma, iota, kappa, gamma, alpha, intercept, gh = N
 #'
 
 solve_mdypl_se4 <- function(kappa, gamma, alpha, intercept, start, gh = NULL, prox_tol = 1e-10, transform = TRUE, ...) {
-  no_int <- 1:3
-  if (transform) {
-    g <- function(pars) {
-      pars[no_int] <- exp(pars[no_int])
-      mdypl_se4(mu = pars[1], b = pars[2], sigma = pars[3], iota = pars[4], kappa = kappa, gamma = gamma, alpha = alpha, intercept = intercept, gh = gh, prox_tol = prox_tol, n_appx = n_appx)
+    no_int <- 1:3
+    if (transform) {
+        g <- function(pars) {
+            pars[no_int] <- exp(pars[no_int])
+            mdypl_se4(mu = pars[1], b = pars[2], sigma = pars[3], iota = pars[4], kappa = kappa, gamma = gamma, alpha = alpha, intercept = intercept, gh = gh, prox_tol = prox_tol, n_appx = n_appx)
+        }
+        start[no_int] <- log(start[no_int])
+    } else {
+        g <- function(pars) {
+            mdypl_se4(mu = pars[1], b = pars[2], sigma = pars[3], iota = pars[4], kappa = kappa, gamma = gamma, alpha = alpha, intercept = intercept, gh = gh, prox_tol = prox_tol, n_appx = n_appx)
+        }
     }
-    start[no_int] <- log(start[no_int])
-  } else {
-    g <- function(pars) {
-      mdypl_se4(mu = pars[1], b = pars[2], sigma = pars[3], iota = pars[4], kappa = kappa, gamma = gamma, alpha = alpha, intercept = intercept, gh = gh, prox_tol = prox_tol, n_appx = n_appx)
-    }
-  }
-  res <- nleqslv(start, g, ...)
-  soln <- if (transform) c(exp(res$x[no_int]), res$x[4]) else res$x
-  attr(soln, "funcs") <- res$fvec
-  attr(soln, "iter") <- res$iter
-  soln
+    res <- nleqslv(start, g, ...)
+    soln <- if (transform) c(exp(res$x[no_int]), res$x[4]) else res$x
+    attr(soln, "funcs") <- res$fvec
+    attr(soln, "iter") <- res$iter
+    soln
 }
